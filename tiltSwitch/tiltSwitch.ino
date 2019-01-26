43462a22df93baaf023d3cd0296287c66c0ab1b4
epetrix <edendawnpetri@gmail.com>
2019-01-26 13:25:08 -0500

    fixed tilt format

--- tiltSwitch/tiltSwitch.ino
+++ tiltSwitch/tiltSwitch.ino
@@ -1,21 +1,24 @@
-int Led = 13 ;/ / define LED Interface
-int buttonpin = 3; / / define the mercury tilt switch sensor interface
-int val ;/ / define numeric variables val
-void setup ()
+int Led = 13; // define LED Interface
+int buttonpin = 3; // define the mercury tilt switch sensor interface
+int val; // define numeric variables val
+
+void setup () 
 {
-pinMode (Led, OUTPUT) ;/ / define LED as output interface
-pinMode (buttonpin, INPUT) ;/ / define the mercury tilt switch sensor output interface
-http://keyes-arduino.taobao.com
+  pinMode (Led, OUTPUT) ; // define LED as output interface
+  pinMode (buttonpin, INPUT) ;
 }
-void loop ()
+
+void loop () 
 {
-val = digitalRead (buttonpin) ;/ / read the values assigned to the digital interface 3 val
-if (val == HIGH) / / When the mercury tilt switch sensor detects a signal, LED flashes
-{
-digitalWrite (Led, HIGH);
-}
-else
-{
-digitalWrite (Led, LOW);
-}
+  val = digitalRead (buttonpin); // read the values assigned to the digital interface 3 val
+  
+  if (val == HIGH) // When the mercury tilt switch sensor detects a signal, LED flashes
+  {
+    digitalWrite (Led, HIGH);
+  }
+
+  else
+  {
+    digitalWrite (Led, LOW);
+  }
 }
