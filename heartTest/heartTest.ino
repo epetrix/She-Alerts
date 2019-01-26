fe027ad4d1deb1ac0211a49333043f646baf7ee7
epetrix <edendawnpetri@gmail.com>
2019-01-26 10:46:22 -0500

    added more heart tester code

--- heartTest/heartTest.ino
+++ heartTest/heartTest.ino
@@ -21,3 +21,30 @@ void loop () {
   oldValue = value;
   delay (period);
 }
+
+int ledc = 10;  
+int sensorPin = 0;
+int period = 100;
+int change, oldvalue, value;
+void setup ()
+{
+  pinMode(ledc, OUTPUT);
+  Serial.begin (9600);
+}
+void loop ()
+{
+  value = analogRead (sensorPin);
+  change = abs(oldvalue - value);
+  if(change >= 2 )
+  {
+    Serial.println (change);
+    analogWrite(ledc, 0); 
+  }
+  else
+  { 
+    analogWrite(ledc,255); 
+  }
+  oldvalue = value;
+  delay (period);
+}
+
