94d008524bf226de54958d3a3199046f34a41e10
colihanvm <vanessa.colihan@gmail.com>
2019-01-26 20:41:11 -0500

    tilt and nd led with wait

--- main/main.ino
+++ main/main.ino
@@ -3,6 +3,11 @@ int button = 5;
 int tiltSensor = 3;
 int heartSensor = 0;
 
+int isTilt = LOW;
+int ledOn = 0;
+
+unsigned long startTime;
+
 void setup() {
   pinMode (led, OUTPUT);
   pinMode (button, INPUT);
@@ -12,6 +17,21 @@ void setup() {
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  isTilt = digitalRead (tiltSensor);
+  if (ledOn == 1) {
+    if (millis() - startTime > 10000) {
+      digitalWrite(led, LOW);
+      ledOn = 0;
+    }
+  } else {
+    if (isTilt == HIGH) {
+      digitalWrite(led, HIGH);
+      ledOn = 1;
+      startTime = millis();
+      delay (5000); //wait for 5 seconds
+    } else {
+      digitalWrite(led, LOW);
+    }
+  }
 
 }
