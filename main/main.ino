0c4bc782f252d5b1bb7d3caf964e42d096b218b9
colihanvm <vanessa.colihan@gmail.com>
2019-01-26 20:24:10 -0500

    added main

---
+++ main/main.ino
@@ -0,0 +1,14 @@
+int led = 6;
+int button = 5;
+int tiltSensor = 3;
+int heartSensor = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
